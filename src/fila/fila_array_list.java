7291c58a7406f35da3d71980ee62926adfd46fd4
Ítalo Gabriel <italogabriel1aaaa@gmail.com>
2022-05-24 15:33:53 -0300

    exception

--- src/fila/fila_array_list.java
+++ src/fila/fila_array_list.java
@@ -1,6 +1,6 @@
 package fila;
-
 import java.util.ArrayList;
+
 
 public class fila_array_list implements IFila {
 
@@ -16,18 +16,26 @@ public class fila_array_list implements IFila {
 
 
 	public void enqueue(Object o) {	
-		a.add(o);
-		
+		a.add(o);	
 	}
 	
-	public Object dequeue() {
-		Object aux = a.get(0);
-		a.remove(0);
-		return aux;
+	public Object dequeue() throws FilaVaziaException {
+		if (isEmpty()) {
+			throw new FilaVaziaException("A Fila está vazia!");
+		} else {
+			Object aux = a.get(0);
+			a.remove(0);
+			return aux;
+		}
+
 	}	
 	
-	public Object first() {
-		return a.get(0);
+	public Object first() throws FilaVaziaException {
+		if (isEmpty()) {
+			throw new FilaVaziaException("A Fila está vazia!");
+		} else {
+			return a.get(0);
+		}
 	}
 
 	public int size() {
