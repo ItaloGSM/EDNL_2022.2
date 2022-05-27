2bd2b8ffa70600c63cd7e6db1ba674d6f4343288
ItaloGSM <italogabriel1aaaa@gmail.com>
2022-05-27 20:11:29 -0300

    Vetor_duplamente_encadeado
    Implementação do TAD Vetor e seus métodos com listas duplamente encadeadas e sua respectiva classe de teste.

--- src/vetor/Vetor_duplamente_ligado.java
+++ src/vetor/Vetor_duplamente_ligado.java
@@ -39,10 +39,50 @@ public class Vetor_duplamente_ligado implements IVetor{
 
 	
 	public Object elemAtRank(int index) throws VetorVazioException {
-		return null;
+		if(index > size() || index < 0) {
+			throw new VetorVazioException("Index inexistente");
+		}
+		if(isEmpty()) {
+			throw new VetorVazioException("Vetor vazio!");
+		}
+		if(index<=(size()/2)){
+			Node_duplamente_encadeado aux = SentinelInicio;
+			for(int i = 0; i<=index;i++) {
+				aux = aux.getNext();
+			}
+		return aux.getElemento();
+		} else {
+			Node_duplamente_encadeado aux = SentinelFim;
+			for(int i = size(); i>index;i--) {
+				aux = aux.getPrev();
+			}
+		return aux.getElemento();
+		}
 	}
 	public Object replaceAtRank(int index, Object o) throws VetorVazioException {
-		return null;
+		if(index > size() || index < 0) {
+			throw new VetorVazioException("Index inexistente");
+		}
+		if(isEmpty()) {
+			throw new VetorVazioException("Vetor vazio!");
+		}
+		if(index<=(size()/2)){
+			Node_duplamente_encadeado aux = SentinelInicio;
+			for(int i = 0; i<=index;i++) {
+				aux = aux.getNext();
+			}
+		Object aux2 = aux.getElemento();
+		aux.setElemento(o);
+		return aux2;
+		} else {
+			Node_duplamente_encadeado aux = SentinelFim;
+			for(int i = size(); i>index;i--) {
+				aux = aux.getPrev();
+			}
+		Object aux2 = aux.getElemento();
+		aux.setElemento(o);
+		return aux2;
+		}
 	}
 	
 	public void insertAtRank(int index, Object o) throws VetorVazioException{
@@ -72,11 +112,11 @@ public class Vetor_duplamente_ligado implements IVetor{
 					aux2.setPrev(new_node);
 					new_node.setNext(aux2);
 				} else {
-					Node_duplamente_encadeado aux = SentinelFim.getPrev();
-					Node_duplamente_encadeado aux2 = SentinelFim.getPrev();
+					Node_duplamente_encadeado aux = SentinelFim;
+					Node_duplamente_encadeado aux2 = SentinelFim;
 					Node_duplamente_encadeado new_node = new Node_duplamente_encadeado();
 					new_node.setElemento(o);
-					for(int i=tamanho ; i>index+1 ; i--) {
+					for(int i=tamanho ; i>index ; i--) {
 						aux = aux.getPrev();
 						aux2 = aux2.getPrev();
 					}
@@ -92,7 +132,33 @@ public class Vetor_duplamente_ligado implements IVetor{
 	}
 	
 	public Object removeAtRank(int index) throws VetorVazioException {
-		return null;
+		if(index > size() || index < 0) {
+			throw new VetorVazioException("Index inexistente");
+		}
+		if(isEmpty()) {
+			throw new VetorVazioException("Vetor vazio!");
+		}
+		if(index<=(size()/2)){
+			Node_duplamente_encadeado aux = SentinelInicio;
+			for(int i = 0; i<=index;i++) {
+				aux = aux.getNext();
+			}
+			Object aux2 = aux.getElemento();
+			aux.getPrev().setNext(aux.getNext());
+			aux.getNext().setPrev(aux.getPrev());
+			tamanho--;
+			return aux2;
+		} else {
+			Node_duplamente_encadeado aux = SentinelFim;
+			for(int i = size(); i>index;i--) {
+				aux = aux.getPrev();
+			}
+			Object aux2 = aux.getElemento();
+			aux.getPrev().setNext(aux.getNext());
+			aux.getNext().setPrev(aux.getPrev());
+			tamanho--;
+			return aux2;
+		}
 	}
 	
 	public int size() {
@@ -106,7 +172,7 @@ public class Vetor_duplamente_ligado implements IVetor{
 	public void mostra() {
 		Node_duplamente_encadeado aux = SentinelInicio.getNext();
 		for(int i = 0; i<tamanho; i++) {
-			System.out.println(aux.getElemento());
+			System.out.println("Index: " + i + " - Elemento: "+ aux.getElemento());
 			aux = aux.getNext();	
 		}
 	}
--- src/vetor/Vetor_encadeado_teste.java
+++ src/vetor/Vetor_encadeado_teste.java
@@ -7,13 +7,39 @@ public class Vetor_encadeado_teste {
 		Vetor_duplamente_ligado v1 = new Vetor_duplamente_ligado();
 		
 		v1.insertAtRank(0, 1);
+		System.out.println("tamanho: "  + v1.size());
+		System.out.println("está vazio ?" + v1.isEmpty());
 		v1.insertAtRank(1, 2);
 		v1.insertAtRank(2, 3);
 		v1.insertAtRank(3, 4);
 		v1.insertAtRank(4, 5);
-		
-		
-		
+		v1.insertAtRank(5, 6);
+		v1.insertAtRank(6, 7);
 		v1.mostra();
+		System.out.println("------------------------------------");
+		System.out.println("tamanho: "  + v1.size());
+		System.out.println("está vazio ?" + v1.isEmpty());
+		v1.insertAtRank(3, 99);
+		v1.mostra();
+		System.out.println("------------------------------------");
+		v1.insertAtRank(7, 66);
+		v1.mostra();
+		System.out.println("------------------------------------");
+		v1.insertAtRank(1, 33);
+		v1.mostra();
+		
+		System.out.println("element at rank 3: " + v1.elemAtRank(3));
+		System.out.println("element at rank 8: " + v1.elemAtRank(8));
+		System.out.println("replace at rank 2: " + "old element: " + v1.replaceAtRank(2, 80));
+		System.out.println("replace at rank 7: " + "old element: " + v1.replaceAtRank(7, 50));
+		System.out.println("------------------------------------");
+		v1.mostra();
+		System.out.println("------------------------------------");
+		System.out.println("remove at rank 2: " + "old element: " + v1.removeAtRank(2));
+		v1.mostra();
+		System.out.println("------------------------------------");
+		System.out.println("remove at rank 6: " + "old element: " + v1.removeAtRank(6));
+		v1.mostra();
+		System.out.println("------------------------------------");
 	}
 }
