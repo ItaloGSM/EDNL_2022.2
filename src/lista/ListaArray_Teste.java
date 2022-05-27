0afd98b8b5d2db915a60f1160003d9ff75f2a549
ItaloGSM <italogabriel1aaaa@gmail.com>
2022-05-27 01:03:07 -0300

    uptade lista_teste
    melhora na classe de teste de lista

--- src/lista/ListaArray_Teste.java
+++ src/lista/ListaArray_Teste.java
@@ -20,21 +20,39 @@ public class ListaArray_Teste {
 		l1.insertLast(99);
 		l1.remove(2);
 		l1.mostra();
-		System.out.println(l1.isFirst(0));
-		System.out.println(l1.isFirst(1));
-		System.out.println(l1.isLast(5));
-		System.out.println(l1.isLast(6));
-		System.out.println(l1.first());
-		System.out.println(l1.last());
+		System.out.println("é o primeiro?" + l1.isFirst(0));
+		System.out.println("é o primeiro?" + l1.isFirst(1));
+		System.out.println("é o ultimo ?" + l1.isLast(5));
+		System.out.println("é o ultimo ?" + l1.isLast(6));
+		System.out.println("primeiro elemento" + l1.first());
+		System.out.println("ultimo elemento" + l1.last());
 		System.out.println("----------------------------------------------------");
 		l1.swapElement(0, 5);
 		l1.replaceElement(4, 100);
 		l1.mostra();
 		System.out.println("----------------------------------------------------");
-		System.out.println(l1.after(2));
-		System.out.println(l1.before(2));
+		System.out.println("antes do indice 2" + l1.after(2));
+		System.out.println("após o indice 2" + l1.before(2));
 		
-		
+		System.out.println("----------------------------------------------------");
+		System.out.println("--------------------------L2--------------------------");
+		ListaArray l2 = new ListaArray(1);
+		l2.insertLast(1);
+		l2.insertLast(2);
+		l2.insertLast(3);
+		l2.insertLast(4);
+		l2.insertLast(5);
+		l2.insertLast(6);
+		l2.mostra();
+		System.out.println("----------------------------------------------------");
+		l2.insertAfter(3, 99);
+		l2.mostra();
+		System.out.println("----------------------------------------------------");
+		l2.insertBefore(2, 66);
+		l2.mostra();
+		System.out.println("----------------------------------------------------");
+		l2.insertFirst(33);
+		l2.mostra();
 	}
 }
 
